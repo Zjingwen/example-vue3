f7171567915c3a53d161d1d385b4f53eed5d2adc
静文（jingwen） <jingwen@xiaoshijie.com>
2020-07-23 01:03:02 +0800

    release: change

---
+++ src/components/button-counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{title}}
+    <button @click="count++">button-counter {{count}}</button>
+  </div>
+</template>
+<script>
+export default{
+  name: 'button-counter',
+  props:['title'],
+  data:()=>({
+    count: 0,
+  }),
+  methods:{
+
+  }
+}
+</script>
--- src/views/base-example.vue
+++ src/views/base-example.vue
@@ -1,8 +1,30 @@
 <template>
-  <div>base-example</div>
+  <div>
+    base-example
+    <button-counter title="My journey with Vue" />
+    <button-counter title="Blogging with Vue" />
+    <button-counter title="Why Vue is so fun" />
+    <hr />
+    <button-counter 
+      v-for="(item) in props"
+      :key="item.id"
+      :title="item.title"
+    />
+  </div>
 </template>
 <script>
+import ButtonCounter from "../components/button-counter.vue";
 export default {
-  name: 'base-example'
-}
+  name: "base-example",
+  data: () => ({
+    props: [
+      { id: 1, title: "My journey with Vue" },
+      { id: 1, title: "Blogging with Vue" },
+      { id: 1, title: "Why Vue is so fun" }
+    ]
+  }),
+  components: {
+    ButtonCounter
+  }
+};
 </script>
