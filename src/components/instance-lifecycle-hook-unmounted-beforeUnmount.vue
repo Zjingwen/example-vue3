f1175dce57b5ea5f70be28e411a4be24d76c6895
静文（jingwen） <jingwen@xiaoshijie.com>
2020-07-22 14:22:58 +0800

    release: change

---
+++ src/components/instance-lifecycle-hook-unmounted-beforeUnmount.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    beforeUpdate/updated/beforeUnmount/unmounted
+  </div>
+</template>
+
+<script>
+export default {
+  name: "handle-unmounted-and-beforeUnmount",
+  beforeUnmount() {
+    console.group("beforeUnmount");
+    console.log(this);
+    console.groupEnd();
+  },
+  unmounted() {
+    console.group("unmounted");
+    console.log(this);
+    console.groupEnd();
+  },
+};
+</script>
--- src/views/instance-lifecycle-hooks.vue
+++ src/views/instance-lifecycle-hooks.vue
@@ -1,11 +1,69 @@
 <template>
   <div>
     <h1>instance-lifecycle-hooks</h1>
+    <handle-unmounted-and-beforeUnmount v-if="show"/>
+    <button @click="handleShow">触发beforeUpdate/updated/beforeUnmount/unmounted</button>
   </div>
 </template>
 
 <script>
+import HandleUnmountedAndBeforeUnmount from '../components/instance-lifecycle-hook-unmounted-beforeUnmount.vue';
 export default {
   name: "instance-lifecycle-hooks",
+  components:{
+    HandleUnmountedAndBeforeUnmount
+  },
+  data:()=>({
+    msg: '0',
+    show: 1
+  }),
+  beforeCreate() {
+    console.group("beforeCreate");
+    console.log(this);
+    console.groupEnd();
+  },
+  created() {
+    console.group("created");
+    console.log(this);
+    console.groupEnd();
+  },
+  beforeMount() {
+    console.group("beforeMount");
+    console.log(this);
+    console.groupEnd();
+  },
+  mounted() {
+    console.group("mounted");
+    console.log(this);
+    console.groupEnd();
+  },
+  beforeUpdate() {
+    console.group("beforeUpdate");
+    console.log(this);
+    console.groupEnd();
+  },
+  updated() {
+    console.group("updated");
+    console.log(this);
+    console.groupEnd();
+  },
+  beforeUnmount() {
+    console.group("beforeUnmount");
+    console.log(this);
+    console.groupEnd();
+  },
+  unmounted() {
+    console.group("unmounted");
+    console.log(this);
+    console.groupEnd();
+  },
+  methods:{
+    handleCount(){
+      this.msg++;
+    },
+    handleShow(){
+      this.show = !this.show;
+    }
+  }
 };
 </script>
