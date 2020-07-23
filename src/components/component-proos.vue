5e1626098df3a4af2be7418ecd6f260d292edc37
静文（jingwen） <jingwen@xiaoshijie.com>
2020-07-23 18:37:25 +0800

    release: change

---
+++ src/components/component-proos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    component-props: {{val}}
+    <br />
+    <input type="text" v-model="v" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'component-props',
+  props:['val'],
+  data:()=>({
+    v: ''
+  }),
+  watch:{
+    v(){
+      // 子组件同步传递给父组件
+      this.$emit('update:foo',`v-emit-component-props: ${this.v}`)
+    },
+    val(){
+      this.$emit('update:foo',`val-emit-component-props: ${this.val}`)
+    }
+  }
+};
+</script>
--- src/views/base-example.vue
+++ src/views/base-example.vue
@@ -5,26 +5,35 @@
     <button-counter title="Blogging with Vue" />
     <button-counter title="Why Vue is so fun" />
     <hr />
-    <button-counter 
-      v-for="(item) in props"
-      :key="item.id"
-      :title="item.title"
-    />
+    <button-counter v-for="(item) in props" :key="item.id" :title="item.title" />
+    <hr />
+    <input type="text" v-model="val" />
+    <component-proos :val="val"  v-model:foo="doSomething" />
+    {{doSomething}}
   </div>
 </template>
 <script>
 import ButtonCounter from "../components/button-counter.vue";
+import ComponentProos from "../components/component-proos.vue";
 export default {
   name: "base-example",
   data: () => ({
+    val: "hello word",
+    doSomething:'',
     props: [
       { id: 1, title: "My journey with Vue" },
       { id: 1, title: "Blogging with Vue" },
-      { id: 1, title: "Why Vue is so fun" }
-    ]
+      { id: 1, title: "Why Vue is so fun" },
+    ],
   }),
   components: {
-    ButtonCounter
-  }
+    ButtonCounter,
+    ComponentProos,
+  },
+  methods: {
+    handleChange(v) {
+      this.doSomething = v;
+    },
+  },
 };
 </script>
