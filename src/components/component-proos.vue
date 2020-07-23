c039426bcd6fc7e60f410b7ef38fb6200cbf12b5
静文（jingwen） <jingwen@xiaoshijie.com>
2020-07-24 00:19:44 +0800

    release: change

--- src/components/component-proos.vue
+++ src/components/component-proos.vue
@@ -1,25 +1,12 @@
 <template>
   <div>
-    component-props: {{val}}
-    <br />
-    <input type="text" v-model="v" />
+    {{title}}
+    <button @click="$emit('enlarge-text', 0.1)">Enlarge text</button>
   </div>
 </template>
 <script>
 export default {
   name: 'component-props',
-  props:['val'],
-  data:()=>({
-    v: ''
-  }),
-  watch:{
-    v(){
-      // 子组件同步传递给父组件
-      this.$emit('update:foo',`v-emit-component-props: ${this.v}`)
-    },
-    val(){
-      this.$emit('update:foo',`val-emit-component-props: ${this.val}`)
-    }
-  }
+  props:['title'],
 };
 </script>
--- src/views/base-example.vue
+++ src/views/base-example.vue
@@ -1,39 +1,44 @@
 <template>
   <div>
-    base-example
+    <p>props传值</p>
     <button-counter title="My journey with Vue" />
     <button-counter title="Blogging with Vue" />
     <button-counter title="Why Vue is so fun" />
     <hr />
-    <button-counter v-for="(item) in props" :key="item.id" :title="item.title" />
+    <p>循环渲染组件</p>
+    <button-counter v-for="(item) in posts" :key="item.id" :title="item.title" />
     <hr />
-    <input type="text" v-model="val" />
-    <component-proos :val="val"  v-model:foo="doSomething" />
-    {{doSomething}}
+    <p>组件props传递 v-model值</p>
+      <div v-bind:style="{fontSize: postFontSize+'em'}">
+        <component-proos 
+          v-for="(item) in posts" 
+          :key="item.id" 
+          :title="item.title" 
+          @enlarge-text="postFontSize += $event" 
+        />
+      </div>
+    <hr />
   </div>
 </template>
 <script>
 import ButtonCounter from "../components/button-counter.vue";
 import ComponentProos from "../components/component-proos.vue";
+
 export default {
   name: "base-example",
   data: () => ({
-    val: "hello word",
-    doSomething:'',
-    props: [
+    posts: [
       { id: 1, title: "My journey with Vue" },
       { id: 1, title: "Blogging with Vue" },
       { id: 1, title: "Why Vue is so fun" },
     ],
+    postFontSize: 1,
   }),
   components: {
     ButtonCounter,
     ComponentProos,
   },
   methods: {
-    handleChange(v) {
-      this.doSomething = v;
-    },
   },
 };
 </script>
